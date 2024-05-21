c01c32e3e07cd502d4ca6c73dabc33d68a88dfd2
Dhivakar-2000 <143002319+Dhivakar-2000@users.noreply.github.com>
2024-05-21 13:29:45 +0530

    Delete LogoutComponent.jsx

--- LogoutComponent.jsx
+++
@@ -1,8 +0,0 @@
-export default function LogoutComponent() {
-    return(
-    <div className="Logout">
-       <h1>You are logged out !!!</h1>
-        <div> Thank you for using our App. Come back soon! </div>
-    </div>
-    )
-}
