d991332521b41b386a872fc1f7c81d8254f7c035
Dhivakar-2000 <143002319+Dhivakar-2000@users.noreply.github.com>
2024-05-21 13:28:39 +0530

    Delete ErrorComponent.jsx

--- ErrorComponent.jsx
+++
@@ -1,8 +0,0 @@
-export default function ErrorComponent() {
-    return(
-    <div className="Error">
-        <h1>We are working really hard !</h1>
-        <div>Please contact our team at HQ</div>
-    </div>
-    )
-}
